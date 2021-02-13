0a339535ba39364d21d77100fb861397d37e58b2
Muddhit Baid <61585314+Mukulbaid63@users.noreply.github.com>
2021-02-14 00:24:10 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,52 @@
-import './App.css';
-import GridItem from './GridItem';
-import React, { Component } from 'react'
+import "./App.css";
+import GridItem from "./GridItem";
+import React, { Component } from "react";
 import GridRow from "./GridRow";
 import Header from "./Header";
 import Footer from "./Footer";
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
+    this.state = {
+      gameState: [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+      ],
+      playerTurn: "X"
+    };
+  }
+  clickHandler = (rowIndex, colIndex) => {
+    const tempGameState = [...this.state.gameState];
 
-    gameState: [
-      ["","",""],
-      ["","",""],
-      ["","",""]
-    ],
-      playerTurn:"X"
+    if (tempGameState[rowIndex][colIndex] !== "") {
+      return;
     }
-  }
-  clickHandler=(rowIndex,colIndex)=>{
-    const tempGameState=[...this.state.gameState]
-    if(this.checkWinner(tempGameState)===true ||tempGameState[rowIndex][colIndex]!=="") {
-      return tempGameState
-       //if the winning condns are satisfied it should return here
 
+    tempGameState[rowIndex][colIndex] = this.state.playerTurn;
 
-  }
-    else {
-    tempGameState[rowIndex][colIndex]=this.state.playerTurn;
     this.setState({
-      gameState:tempGameState,
-      playerTurn:this.state.playerTurn==="X"?"O":"X"
-    })
-  }
-  
-}
-  
-  checkWinner=(tempState)=>{
-    
-    const [a,b,c]=tempState[0];
-    const [d,e,f]=tempState[1];
-    const [g,h,i]=tempState[2];
+      gameState: tempGameState,
+
+      playerTurn: this.state.playerTurn === "X" ? "O" : "X"
+    });
+
+    if (this.checkWinner(tempGameState)) {
+      this.setState({
+        playerTurn: ""
+      });
+      console.log("WIN");
+
+      return <p>{this.state.playerTurn} wins</p>;
+
+      //if the winning condns are satisfied it should return here
+    }
+  };
+
+  checkWinner = (tempState) => {
+    const [a, b, c] = tempState[0];
+    const [d, e, f] = tempState[1];
+    const [g, h, i] = tempState[2];
 
     return (
       this.checkThree(a, b, c) ||
@@ -50,33 +57,34 @@ class App extends Component {
       this.checkThree(c, f, i) ||
       this.checkThree(a, e, i) ||
       this.checkThree(c, e, g)
-    )
-
-  }
-  checkThree=(a, b, c)=>{
-    if (a!=="" || b!=="" || c!=="") return false
-    return a === b && b === c
-  }
+    );
+  };
+  checkThree = (a, b, c) => {
+    if (a === "" || b === "" || c === "") return false;
+    return a === b && b === c;
+  };
   render() {
     return (
       <div className="container">
-       <Header/>
-     
-      <div id="board">
-      {this.state.gameState.map((row,rowIndex)=>(
-        <GridRow row={row} key={rowIndex} rowIndex={rowIndex} clickHandler={this.clickHandler}/>
-      ))}
-      </div>
-      {/* <GridRow/>
+        <Header />
+
+        <div id="board">
+          {this.state.gameState.map((row, rowIndex) => (
+            <GridRow
+              row={row}
+              key={rowIndex}
+              rowIndex={rowIndex}
+              clickHandler={this.clickHandler}
+            />
+          ))}
+        </div>
+        {/* <GridRow/>
       <GridRow/>
       <GridRow/> */}
-      <Footer turn={this.state.playerTurn}/>
-        
+        <Footer turn={this.state.playerTurn} />
       </div>
-    )
+    );
   }
 }
-
-
 
 export default App;
